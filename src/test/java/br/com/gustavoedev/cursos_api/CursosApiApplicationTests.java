fb1621786f931b8f6437a450fa1384146cf8a3e2
GustavoeDev <gustavo.queiroz@nees.ufal.br>
2025-10-06 21:54:21 -0300

    initial commit

---
+++ src/test/java/br/com/gustavoedev/cursos_api/CursosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavoedev.cursos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
